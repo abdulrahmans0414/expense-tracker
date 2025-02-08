2bbd21cbcb5f4bb5c9571fc4546a56ad94797dda
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-02-08 18:44:46 +0530

    Add expense form submission logic

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const expenseForm = document.getElementById("expense-form")
+  const expenseNameInput = document.getElementById("expense-name")
+  const expenseAmountInput = document.getElementById("expense-amount")
+
+  const expenseList = document.getElementById("expense-list")
+  const totalAmountDisplay = document.getElementById("total-amount")
+
+  let expenses = []
+  let totalAmount = calculateTotal()
+
+  // how to add 
+  expenseForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const name = expenseNameInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value);
+
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+      const newExpense = {
+        id: Date.now(),
+        name: name,
+        amount: amount,
+      }
+      expenses.push(newExpense);
+      saveExpensesTolocal();
+
+
+    }
+
+  })
+
+
+
+  function calculateTotal() {
+
+  }
+
+});
