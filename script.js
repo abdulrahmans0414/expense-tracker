a99ec965ab79cbbc4df8f96908d6f6fea7116d1b
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-02-09 16:19:08 +0530

    add functionlity in javascript redreder expense and updatetotalexpence and save expenses to local storage

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseList = document.getElementById("expense-list")
   const totalAmountDisplay = document.getElementById("total-amount")
 
-  let expenses = []
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || []
   let totalAmount = calculateTotal()
+
+  renderExpenses();
 
   // how to add 
   expenseForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const name = expenseNameInput.value.trim();
+    // every single input comes with strings 
     const amount = parseFloat(expenseAmountInput.value);
 
     if (name !== "" && !isNaN(amount) && amount > 0) {
@@ -25,16 +28,50 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       expenses.push(newExpense);
       saveExpensesTolocal();
+      renderExpenses()
+      updateTotalExpenses();
 
-
+      // clear input 
+      expenseNameInput.value = '';
+      expenseAmountInput.value = '';
     }
 
   })
 
 
+  function renderExpenses() {
+    expenseList.innerHTML = '';
+    expenses.forEach(expense => {
+      const li = document.createElement('li')
+      li.innerHTML = `
+        ${expense.name} - $${expense.amount}
+        <button data-id="${expense.id}">Delete</button>
+        `;
+      expenseList.appendChild(li);
+    });
+  }
 
   function calculateTotal() {
-
+    return expenses.reduce((sum, expense) => (sum + expense.amount), 0);
   }
+  function saveExpensesTolocal() {
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  }
+
+  function updateTotalExpenses() {
+    totalAmount = calculateTotal();
+    totalAmountDisplay.textContent = totalAmount.toFixed(2);
+  }
+
+  expenseList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const expenseId = parseInt(e.target.getAttribute("data-id"));
+      expenses = expenses.filter((expense) => expense.id !== expenseId);
+
+      saveExpensesTolocal();
+      renderExpenses();
+      updateTotalExpenses();
+    }
+  })
 
 });
